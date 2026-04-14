770666d5b20b80b92209be84c58d10961f7de2d0
DomaGamerRDG <ress.dominik.ferenc@students.jedlik.eu>
2026-04-14 11:12:41 +0200

    fix: downgrade eslint to version 9.7.0 and remove unused eslint/js dependency; update vite config to set server port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {   // Így CORS nélkül hívhatod az API-t.
-      '/api': 'http://localhost:3000'
-    }
+    port: 5173,
   }
 })
